6748fef858d12a4cac470b2a5d263b279183eaab
Calvin Flegal <cflegal@digitallumens.com>
2015-12-15 09:52:48 -0500

    go back

--- Slider.js
+++ Slider.js
@@ -324,8 +324,8 @@ var Slider = React.createClass({
     var touchOverflowSize = this._getTouchOverflowSize();
 
     return new Rect(
-      touchOverflowSize.height / 2 + (state.containerSize.height - props.thumbTouchSize.height) / 2,
       touchOverflowSize.width / 2 + this._getThumbLeft(state.value) + (state.thumbSize.width - props.thumbTouchSize.width) / 2,
+      touchOverflowSize.height / 2 + (state.containerSize.height - props.thumbTouchSize.height) / 2,
       props.thumbTouchSize.width,
       props.thumbTouchSize.height
     );
